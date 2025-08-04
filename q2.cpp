38d488b8fed8b47cd1210df67bb6e3ae2d891de5
kohsheenn <kohsheenrazdan50716@gmail.com>
2025-08-04 19:35:23 +0530

    assignment_1b

---
+++ q2.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows, cols;
+    int matrix[100][100], transpose[100][100];
+
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+
+    printf("\nTranspose of the matrix:\n");
+    for (int i = 0; i < cols; i++) {
+        for (int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
